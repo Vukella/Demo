f52ce9b2ded5c7aa93699fb79b89ca259c35a339
Vukella <nemanja.vukelic@outlook.com>
2024-05-10 22:55:17 +0200

    Adds factory files

---
+++ database/factories/BlogFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Blog;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Blog>
+ */
+class BlogFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = Blog::class;
+    public function definition(): array
+    {
+        return [
+            //
+            'user_id' => User::factory(),
+            'slug' => $this->faker->unique()->slug(),
+            'title' => $this->faker->unique()->title(),
+            'body' => '<p>' . implode('</p><p>',$this->faker->paragraphs(6)), '</p>',
+            'summary' => '<p>' . implode('</p><p>',$this->faker->paragraphs(1)), '</p>',
+            'published_at' => now()
+
+        ];
+    }
+}
