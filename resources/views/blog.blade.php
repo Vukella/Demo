1eae472153c6f3c40c5e19c7b54e23458417b2ef
Vukella <nemanja.vukelic@outlook.com>
2024-05-21 08:20:54 +0200

    Deletes welcome file, renames blog-details to blogs, starts with changing those files to dinamic code

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -4,12 +4,12 @@
 <x-head></x-head>
 
 <body>
-
+    
 <x-navbar></x-navbar>
 
 <!-- breadcrumb area start -->
 <x-breadcrumb_area>
-    Blog
+    Blog Details
 </x-breadcrumb_area>
 <!-- breadcrumb area end -->
 
@@ -17,32 +17,49 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-8">
-                <div class="row">
-                    <x-single_blog_grid_item>
-                        Mobile apps are becoming popular in 3rd world
-                    </x-single_blog_grid_item>
-                    <x-single_blog_grid_item>
-                        Depart do be so he enough talent. Sociable for
-                    </x-single_blog_grid_item>
-                    <x-single_blog_grid_item>
-                        At name no an what Pressed my by do affixed
-                    </x-single_blog_grid_item>
-                    <x-single_blog_grid_item>
-                        Do am he horrible distance marriage so alth
-                    </x-single_blog_grid_item>
-                    <x-single_blog_grid_item>
-                        His many same been well can high that did law
-                    </x-single_blog_grid_item>
-                    <x-single_blog_grid_item>
-                        He into walk roof made tall cold he jelings
-                    </x-single_blog_grid_item>
-
-                    <x-blog_pagination></x-blog_pagination>
-                </div>
+                    <div class="single-post-details-item"><!-- blog single content -->
+                        <div class="thumb">
+                            <img src="assets/img/blog/blog-details.jpg" alt="blog image">
+                        </div>
+                        <div class="entry-content">
+                            <ul class="post-meta">
+                                <li><a href="#"><i class="far fa-calendar-alt"></i> 12-01-2019</a></li>
+                                <li><a href="#"><i class="far fa-user"></i> Admin</a></li>
+                                <li class="cat"><i class="fas fa-tags"></i> <a href="#">Lifestyle</a> <a href="#">Travel</a><a href="#">Music</a></li>
+                            </ul>
+                            <h4 class="title">Did even but nor are most gave hope depend son repair day ladies now  </h4>
+                            <p>Lorem Ipsums is simply dummy text of the print  industry. Lorem Ipsum has industry's standard dummy text ever since. Lorem Ipsums is simply dummy text of the print  industry. Lorem Ipsum has industry's standard dummy text ever since.</p>
+                            <p>Proin vitae blandit felis. Proin rhoncus imperdiet facilisis. Etiam et mauris ex. Maecenas posuere ipsum orci, at imperdiet est venenatis quis. Maecenas quis fermentum ipsum, ac eleifend urna. Cras sed viverra nibh. Mauris eget finibus erat. Mauris tempor varius purus nec cursus. Nullam ornare eget ipsum sit amet consequat. Nunc finibus vitae diam non suscipit. Praesent elementum felis sit amet urna tempus commodo. Proin congue id ante ac maximus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec euismod arcu venenatis, viverra arcu eu, efficitur mauris. Aliquam sagittis sagittis elit, in cursus est faucibus in.</p>
+                            <blockquote>
+                                    Nunc blandit tincidunt consequat. Duis diam metus, suscipit ininar eget, egestas id arcu. Duis a enim vel mauris ultrices. Nullam aliquet velit ac velit tempus in semper.
+                            </blockquote>
+                            <p>Lorem Ipsums is simply dummy text of the print  industry. Lorem Ipsum has industry's standard dummy text ever since. Lorem Ipsums is simply dummy text of the print  industry. Lorem Ipsum has industry's standard dummy text ever since.</p>
+                            <p>Proin vitae blandit felis. Proin rhoncus imperdiet facilisis. Etiam et mauris ex. Maecenas posuere ipsum orci, at imperdiet est venenatis quis. Maecenas quis fermentum ipsum, ac eleifend urna. Cras sed viverra nibh. Mauris eget finibus erat. Mauris tempor varius purus nec cursus.</p>
+                        </div>
+                        <div class="entry-footer"><!-- entry footer -->
+                            <div class="left">
+                                <ul class="tags">
+                                    <li class="title">Tags: </li>
+                                    <li><a href="#">Marketing</a><a href="#">Design</a></li>
+                                </ul>
+                            </div>
+                            <div class="right">
+                                <ul class="social-share">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                </ul>
+                            </div>
+                        </div><!-- //. entry footer -->
+    
+                        <x-comment_area></x-comment_area>
+    
+                        <x-comment_form_wrap></x-comment_form_wrap>
+                    </div> 
             </div>
-        <x-widget_area>
-            
-        </x-widget_area>
+            <x-widget_area></x-widget_area>
+        </div>
     </div>
 </div>
 
